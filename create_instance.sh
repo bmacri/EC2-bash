ad9fe2748988e64d260d0377a06b2fc973cc4d6a
Bethany Macri <bethany.macri@gmail.com>
2012-06-23 13:30:25 -0400

    Correctd the run-instances command.  The script now launches an ec2 machine.

--- create_instance.sh
+++ create_instance.sh
@@ -10,28 +10,30 @@ then
 	#Assign the cert-CERT.pem file to EC2_CERT_PATH
 
 	echo "Enter the absolute path of your public key (cert-CERT.pem): "
-	read  EC2_CERT_PATH
-	export EC2_CERT=$EC2_CERT_PATH
+	#read  EC2_CERT_PATH
+	EC2_CERT=~/Downloads/cert-ECUEYWPG75LWAW5ZDHNRTYLER2NTP7MJ.pem
 
 	#Assign the pk-PK.pem file to EC2_PRIVATE_KEY
 	echo "Enter the absolute path of your private key (pk-PK.pem): "
-        read  EC2_PRIVATE_KEY
-        export EC2_PRIVATE_KEY=$EC2_PRIVATE_KEY
+        #read  EC2_PRIVATE_KEY
+        EC2_PRIVATE_KEY=~/Downloads/pk-ECUEYWPG75LWAW5ZDHNRTYLER2NTP7MJ.pem
 
 	#Ask the user to choose an ami for their instance
 	echo "Please choose an ami from the following list.  Enter the ami below.  These ami options will also be saved in amis.txt in the current directory."
-	ec2-describe-images -o amazon | tee ./amis.txt
+	ec2-describe-images -K $EC2_PRIVATE_KEY -o amazon | tee ./amis.txt
+	echo ec2-describe-images -K $EC2_PRIVATE_KEY -o amazon 
 	read USER_AMI_CHOICE
-
+	
 	#launch the machine
-	ec2-run-instances $USER_AMI_CHOICE -K $EC2_PRIVATE_KEY
-
-	#all ports are blocked by default - authorize port 80 (http) and port 22 (ssh)
+	echo ec2-run-instances $USER_AMI_CHOICE -K $EC2_PRIVATE_KEY -C $EC2_CERT
+	ec2-run-instances $USER_AMI_CHOICE -K $EC2_PRIVATE_KEY -C $EC2_CERT 	#all ports are blocked by default - authorize port 80 (http) and port 22 (ssh)
 	ec2-authorize default -p 22
 	ec2-authorize default -p 80
 
 	#ask the user to input the url - need it to ssh into the instance
 	echo "Please enter the url you kept track of from earlier: "
 	read EC2_URL
-	ssh -i $EC2_URL -K $EC2_PRIVATE_KEY -C $EC2_CERT
+	ssh $EC2_URL -i $EC2_PRIVATE_KEY 
 fi
+
+#sed 's/.*ami.[0-9a-z]*[ \t]*//' | sed 's/ip.*//' | tail -n 1`
